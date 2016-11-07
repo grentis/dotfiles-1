c74328f262027c025dad213728d3bb53d548e50f
grentis <grentis@gmail.com>
2016-11-07 16:35:18 +0100

    tomcat

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,7 @@
 alias reload!='. ~/.zshrc'
-alias tomcat-="ps ax | grep tomcat | grep -v grep"
-alias tomcat!="sudo kill -9 `tomcat! | cut -d'?' -f1`"
+alias tomcat-="ps ax | grep tomcat | grep -v grep | cut -d' ' -f1"
+alias tomcat!="sudo kill -9 `tomcat-`"
+alias start='start-tomcat'
+alias stop='stop-tomcat'
+alias log='log-tomcat'
+alias show='show-tomcat'
